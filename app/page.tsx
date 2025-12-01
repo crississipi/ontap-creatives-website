47b67b90713eca8ee60b37eedf2064714ecc1de2
crississsipi <crismalipico12@gmail.com>
2025-12-01 17:07:10 +0800

    Version 3.6 [Trial For Admin Page]

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
 "use client";
 
 import { useEffect, useState } from "react";
 import { Mainpage } from "@/components";
-import { Page } from "@/components/admin";
 import { canUseSecretKeyAccess, refreshStoredRoleSession } from "@/utils/adminAccessSession";
+import { AdminLogin } from "@/components/admin";
 
-=======
-import { Mainpage } from "@/components";
->>>>>>> ebf4a206820da091b50990d7f9eb3550ad0230a6
 export default function Home() {
   const editable = false;
+  const [adminLogin, showAdminLogin] = useState(false);
+  const [page, setPage] = useState(0)
 
-<<<<<<< HEAD
   useEffect(() => {
     refreshStoredRoleSession();
   }, []);
@@ -35,15 +32,10 @@ export default function Home() {
   return (
     <main className='min-h-[100vh] h-auto w-full flex flex-col items-center relative overflow-x-hidden p-0 m-0 select-none'>
       {adminLogin ? (
-        <Page showAdminLogin={showAdminLogin} editable={editable} />
+        <AdminLogin showAdminLogin={showAdminLogin} setPage={setPage}/>
       ) : (
         <Mainpage editable={editable} />
       )}
-=======
-  return (
-    <main className='min-h-[100vh] h-auto w-full flex flex-col items-center relative overflow-x-hidden p-0 m-0 select-none'>
-      <Mainpage editable={editable} />
->>>>>>> ebf4a206820da091b50990d7f9eb3550ad0230a6
     </main>
     
   );
