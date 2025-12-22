3217f3610d8b3ecc3f477dc44044ea1019759387
crississsipi <crismalipico12@gmail.com>
2025-12-22 15:05:03 +0800

    Version 4.2.24 [Last Modification]

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@
 import { Mainpage } from "@/components";
 
 export default function Home() {
-  const editable = false;
 
   return (
     <main className='min-h-screen h-auto w-full flex flex-col items-center relative overflow-x-hidden p-0 m-0 select-none'>
-      <Mainpage editable={editable} />
+      <Mainpage />
     </main>
     
   );
